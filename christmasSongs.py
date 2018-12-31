33b2e1033960d6340eba8582ca0fb693c26b8d68
Chad Heise <chad@heise.org>
2018-12-31 10:10:35 -0800

    Reduce lines

--- christmasSongs.py
+++ christmasSongs.py
@@ -1,17 +1,6 @@
-import string
-
-songs = ["Rudolph the Red Nosed Reindeer", "Frosty the Snowman", "I'll Be Home for Christmas", "Jingle Bell Rock", \
-"Joy to the World", "Jolly Old St Nicholas", "Up on the Housetop", "White Christmas", "Silver Bells" \
-"Winter Wonderland", "Let It Snow", "O Holy Night", "The First Noel", "Oh Christmas Tree", "Jingle Bells", "The Twelve Days of Christmas"]
-songs = [s.replace(' ','').upper() for s in songs]
-
-allLetterCounts = {letter:0 for letter in (string.ascii_uppercase + "'") }
-
-for s in songs:
-    wordLetterCounts = {letter:0 for letter in (string.ascii_uppercase + "'") }
-    for letter in s:
-        wordLetterCounts[letter] += 1
-    allLetterCounts = {letter:(wordLetterCounts[letter],allLetterCounts[letter]) [ wordLetterCounts[letter] < allLetterCounts[letter] ] for letter in wordLetterCounts}
-            
+allLetterCounts = {letter:0 for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ'" }
+for s in [ s.replace(' ','').upper() for s in ["Rudolph the Red Nosed Reindeer", "Frosty the Snowman", "I'll Be Home for Christmas", "Jingle Bell Rock", "Joy to the World", "Jolly Old St Nicholas", "Up on the Housetop", "White Christmas", "Silver Bells", "Winter Wonderland", "Let It Snow", "O Holy Night", "The First Noel", "Oh Christmas Tree", "Jingle Bells", "The Twelve Days of Christmas"] ]:
+    wordLetterCounts = {letter:s.count(letter) for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ'" }
+    allLetterCounts = {letter:(wordLetterCounts[letter],allLetterCounts[letter]) [ wordLetterCounts[letter] < allLetterCounts[letter] ] for letter in wordLetterCounts}                        
 for letter in allLetterCounts:
     print(letter + " = " + str(allLetterCounts[letter]))
